0c4dcae94ffd27812d3805b811f5b984d2aa7693
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-01-06 13:47:06 -0500

    Added redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,49 +1,48 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
-    const [formData,setFormData] = useState({})
-    const [error,setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({});
+    const { loading, error } = useSelector(state => state.user);
+    //const temp = useSelector((state) => state.user);
+   // console.log(temp);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const handleChange = (e) => {
-        setFormData(
-            {
-                ...formData,
-                [e.target.id]: e.target.value,
-            }
-        );
-      //  console.log(formData)
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
     };
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-        setLoading(true);
-        const res = await fetch('/api/auth/signin', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json',
-            },
-            body: JSON.stringify(formData),
+      e.preventDefault();
+      try {
+        dispatch(signInStart());
+        const res = await fetch('/api/auth/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
         });
         const data = await res.json();
         console.log(data);
-        if(data.success === false) {
-            setError(data.message);
-            setLoading(false);
-            return
+        if (data.success === false) {
+          dispatch(signInFailure(data.message));
+          return;
         }
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data));
         navigate('/');
-        console.log(data); 
-    } catch (error){
-        setLoading(false);
-        setError(error.message);
-    }
-    } 
+      } catch (error) {
+        dispatch(signInFailure(error.message));
+      }
+    };
     //console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
