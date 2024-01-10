9e4acbe10ea3c0b2384157f8a0b6a28547011c06
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-01-10 13:32:43 -0500

    create search api route

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing, getListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, updateListing, getListings } from '../controllers/listing.controller.js';
 
 export const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get',getListings);
+
 
 export default router;
